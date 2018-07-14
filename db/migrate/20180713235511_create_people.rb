9b25753ddeb386c0ff0fa103d74c695f2fbbff1e
Agustin Silva <silva.agustinn@gmail.com>
2018-07-13 21:04:37 -0300

    Initial commit.

---
+++ db/migrate/20180713235511_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Person.delete_all
+
+10.times do
+  Person.create(
+    name: Faker::Name.name,
+    phone: Faker::PhoneNumber.cell_phone,
+    email: Faker::Internet.email
+  )
+end
